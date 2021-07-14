f36c3cf4f95a9bd7ee2578320b61c23c1e5837d4
bill.mark <2286449127@qq.com>
2021-07-14 17:20:49 +0800

    update

--- autobuild.sh
+++ autobuild.sh
@@ -1,18 +1,20 @@
+
+# 前端在服务器的git地址
 WEB_PATH='/var/shop_admin_fe/'
+
 WEB_USER='root'
 WEB_USERGROUP='root'
 
-echo "Start deployment"
+echo "Start"
 cd $WEB_PATH
 echo `pwd`
 echo "pulling source code..."
 git fetch --all
 git reset --hard origin/main
 git pull
-echo "changing permissions..."
+echo "next build..."
 yarn
 npm run build
 echo "build end"
 #chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
 #echo "Finished."
-
